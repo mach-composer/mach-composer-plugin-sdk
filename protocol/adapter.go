7679933d2df4ccda7f49e0febace9ab52192c91a
Michael van Tellingen <michaelvantellingen@gmail.com>
2022-12-02 13:08:22 +0100

    Make sure we only call plugin methods that are defined
    We never want to call a method from the plugin framework that is not
    defined in the Plugin Schema. Add various not nil checks for this

--- protocol/adapter.go
+++ protocol/adapter.go
@@ -41,34 +41,34 @@ func (p *Adapter) IsEnabled() bool {
 }
 
 func (p *Adapter) SetRemoteStateBackend(data map[string]any) error {
-	if p.fn.IsEnabled != nil && p.fn.SetRemoteStateBackend != nil {
+	if p.fn.SetRemoteStateBackend != nil {
 		return p.fn.SetRemoteStateBackend(data)
 	}
 	return nil
 }
 
 func (p *Adapter) SetGlobalConfig(data map[string]any) error {
-	if p.fn.IsEnabled != nil && p.fn.SetGlobalConfig != nil {
+	if p.fn.SetGlobalConfig != nil {
 		return p.fn.SetGlobalConfig(data)
 	}
 	return nil
 }
 func (p *Adapter) SetSiteConfig(site string, data map[string]any) error {
-	if p.fn.IsEnabled != nil && p.fn.SetSiteConfig != nil {
+	if p.fn.SetSiteConfig != nil {
 		return p.fn.SetSiteConfig(site, data)
 	}
 	return nil
 }
 
 func (p *Adapter) SetSiteComponentConfig(site string, component string, data map[string]any) error {
-	if p.fn.IsEnabled != nil && p.fn.SetSiteComponentConfig != nil {
+	if p.fn.SetSiteComponentConfig != nil {
 		return p.fn.SetSiteComponentConfig(site, component, data)
 	}
 	return nil
 }
 
 func (p *Adapter) SetSiteEndpointsConfig(site string, data map[string]any) error {
-	if p.fn.IsEnabled != nil {
+	if p.fn.SetSiteEndpointsConfig != nil {
 		return p.fn.SetSiteEndpointsConfig(site, data)
 	}
 	return nil
@@ -88,28 +88,28 @@ func (p *Adapter) SetComponentEndpointsConfig(component string, endpoints map[st
 	return nil
 }
 func (p *Adapter) RenderTerraformStateBackend(site string) (string, error) {
-	if p.fn.IsEnabled != nil {
+	if p.fn.RenderTerraformStateBackend != nil {
 		return p.fn.RenderTerraformStateBackend(site)
 	}
 	return "", nil
 }
 
 func (p *Adapter) RenderTerraformProviders(site string) (string, error) {
-	if p.fn.IsEnabled != nil && p.fn.RenderTerraformProviders != nil {
+	if p.fn.RenderTerraformProviders != nil {
 		return p.fn.RenderTerraformProviders(site)
 	}
 	return "", nil
 }
 
 func (p *Adapter) RenderTerraformResources(site string) (string, error) {
-	if p.fn.IsEnabled != nil && p.fn.RenderTerraformResources != nil {
+	if p.fn.RenderTerraformResources != nil {
 		return p.fn.RenderTerraformResources(site)
 	}
 	return "", nil
 }
 
 func (p *Adapter) RenderTerraformComponent(site, component string) (*schema.ComponentSchema, error) {
-	if p.fn.IsEnabled != nil && p.fn.RenderTerraformComponent != nil {
+	if p.fn.RenderTerraformComponent != nil {
 		return p.fn.RenderTerraformComponent(site, component)
 	}
 	return nil, nil
