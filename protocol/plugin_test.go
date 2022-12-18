07134320504e14ecdc6c640d12f05be8df989541
Michael van Tellingen <michaelvantellingen@gmail.com>
2022-12-18 10:35:36 +0100

    Add simple test case for rpc

---
+++ protocol/plugin_test.go
@@ -0,0 +1,37 @@
+package protocol
+
+import (
+	"testing"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/mach-composer/mach-composer-plugin-sdk/schema"
+)
+
+func TestPluginRPC(t *testing.T) {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.LevelFromString("DEBUG"),
+		JSONFormat: true,
+	})
+
+	adapter := NewAdapter(&schema.PluginSchema{
+		Identifier: "test",
+	}, logger)
+
+	var pluginMap = map[string]plugin.Plugin{
+		"MachComposerPlugin": &Plugin{Adapter: adapter, Logger: logger},
+	}
+	client, _ := plugin.TestPluginRPCConn(t, pluginMap, nil)
+	defer client.Close()
+
+	raw, err := client.Dispense("MachComposerPlugin")
+	require.NoError(t, err)
+
+	plugin, ok := raw.(schema.MachComposerPlugin)
+	require.True(t, ok)
+
+	assert.Equal(t, plugin.Identifier(), "test")
+}
